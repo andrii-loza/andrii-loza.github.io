9b97276a44bd765394108293b0ec78ca1c4e90fc
Andrii Loza <loza.andriy@wetelo.com>
2020-09-27 17:31:20 +0300

    add birthday cron job

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
         var $container = $('#container'),
             $filterLinks = $('#filters button');
 
+        setInterval(() => {
+            let ageVar = document.getElementById('age');
+            const bDay = new Date('1997-03-07');
+            ageVar.innerHTML = calculateAge(bDay);
+        }, 1000*60*60*24);
+
         $container.isotope({
             itemSelector: '.item',
             filter: '*'
@@ -25,5 +31,10 @@ $(document).ready(function () {
             });
         });
 
+        const calculateAge = (bDay) => {
+            const ageDifMs = Date.now() - bDay.getTime();
+            const ageDate = new Date(ageDifMs);
+            return Math.abs(ageDate.getUTCFullYear() - 1970);
+        }
     });
 });
